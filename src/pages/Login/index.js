6aa2f8c1842b541b487a56b617b4fe430b07db53
Muhammad Rizki <rizkielshirazy@gmail.com>
2020-06-07 20:32:08 +0700

    implement loading function

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,47 +1,97 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { showMessage } from 'react-native-flash-message';
 import { ILLogo } from '../../assets';
-import { Button, Gap, Input, Link } from '../../components';
-import { colors, fonts } from '../../utils';
+import { Button, Gap, Input, Link, Loading } from '../../components';
+import { Fire } from '../../config';
+import { colors, fonts, storeData, useForm } from '../../utils';
 
 const Login = ({ navigation }) => {
+  const [form, setForm] = useForm({ email: '', password: '' });
+  const [loading, setLoading] = useState(false);
+
+  const login = () => {
+    console.log('form', form);
+    setLoading(true);
+
+    // Authentication user with email and password
+    Fire.auth()
+      .signInWithEmailAndPassword(form.email, form.password)
+      .then((res) => {
+        // Get user data from database
+        Fire.database()
+          .ref(`users/${res.user.uid}/`)
+          .once('value')
+          .then((resDB) => {
+            if (resDB.val()) {
+              // Store data to local storage
+              setLoading(false);
+              storeData(resDB.val());
+              navigation.reset({ index: 0, routes: [{ name: 'MainApp' }] });
+            }
+          });
+      })
+      .catch((err) => {
+        setLoading(false);
+        showMessage({
+          message: err.message,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      });
+  };
   return (
-    <View style={styles.container}>
-      <ILLogo />
-      <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
-      <View>
-        <Input label="Email Address" />
-        <Gap height={24} />
-        <Input label="Password" />
-        <Gap height={10} />
-        <Link title="Forgot My Password" size={12} />
-        <Gap height={40} />
-        <Button
-          title="Sign In"
-          onPress={() =>
-            navigation.reset({ index: 0, routes: [{ name: 'MainApp' }] })
-          }
-        />
-        <Gap height={30} />
-        <Link
-          title="Create New Account"
-          size={16}
-          align="center"
-          onPress={() => navigation.navigate('Register')}
-        />
-      </View>
-    </View>
+    <>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          <ILLogo />
+          <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+          <View>
+            <Input
+              label="Email Address"
+              value={form.email}
+              onChangeText={(value) => setForm('email', value.trim())}
+            />
+            <Gap height={24} />
+            <Input
+              label="Password"
+              value={form.password}
+              onChangeText={(value) => setForm('password', value)}
+              secureTextEntry
+            />
+            <Gap height={10} />
+            <Link title="Forgot My Password" size={12} />
+            <Gap height={40} />
+            <Button title="Sign In" onPress={login} />
+            <Gap height={30} />
+            <Link
+              title="Create New Account"
+              size={16}
+              align="center"
+              onPress={() => navigation.navigate('Register')}
+            />
+          </View>
+        </View>
+      </ScrollView>
+
+      {loading && <Loading />}
+    </>
   );
 };
 
 export default Login;
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 40, justifyContent: 'space-between' },
+  container: {
+    flex: 1,
+    padding: 40,
+  },
   title: {
     fontSize: 20,
     fontFamily: fonts.primary[600],
     color: colors.text.primary,
     maxWidth: 153,
+    marginVertical: 30,
   },
 });
